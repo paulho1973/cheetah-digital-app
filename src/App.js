2f610dad37618ccfe7933781bac0c980c3b14137
YONG HING HO <paulho1973@outlook.com>
2023-02-02 20:07:20 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import data from './data.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    const list = [];
+
+    for (let i = 0; i < data.recipients.length; i++) {
+  
+        for (let j = i+1; j < data.recipients.length; j++) {
+
+            let name1 = data.recipients[i].name;
+            let name2 = data.recipients[j].name;
+            // find intersection in tags
+            let commontags = data.recipients[i].tags.filter(value => data.recipients[j].tags.includes(value));
+
+            if (commontags.length > 0) {
+                list.push(name1 + ", " + name2 + " - [" + commontags.toString() + "] | ")
+            }
+
+        }
+    }
+
+    if (list) console.log(list)
+
+    return (
+        <>
+        <div>{list}</div>
+        </>
+    );
+  }
+  
+  export default App;
